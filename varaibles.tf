96888cf31bfd321aa2496a6ed69f21714663b94e
bishal-rai <160552215+bishal-rai@users.noreply.github.com>
2024-02-22 10:37:55 -0500

    Update varaibles.tf

--- varaibles.tf
+++ varaibles.tf
@@ -4,6 +4,11 @@ variable "bucket" {
   description = "Name of the bucket"
 }
 
+variable "awsRegion" {
+  type        = string
+  description = "Aws Region to apply"
+}
+
 variable "s3_tags" {
   type        = map(any)
   description = "Tags for S3"

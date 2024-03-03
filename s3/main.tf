fbeb07a4dcccd1aa376e6543be397713d3fddc9c
CACI\Bishal.Chamling <Bishal.Chamling@caci.com>
2024-03-03 18:26:03 -0500

    Clean up code

--- s3/main.tf
+++ s3/main.tf
@@ -3,11 +3,4 @@ resource "aws_s3_bucket" "s3_buckets" {
   tags   = var.s3_tags
 }
 
-# resource "aws_s3_bucket_versioning" "s3_buckets_versioning" {
-#   for_each = { for s in var.s3_bucket_props : s.bucket => s }
-#   bucket   = aws_s3_bucket.s3_buckets[each.value.bucket].id
-#   versioning_configuration {
-#     status = each.value.versioning
-#   }
-# }
 
